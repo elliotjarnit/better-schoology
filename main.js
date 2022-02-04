f51abd6f5321627e89ac1696a1eca5017a0c0d15
Swiftzerr <swiftzermc@gmail.com>
2022-02-03 20:49:18 -0500

    made the app load the html in main.html

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 let electron = require("electron");
 
 let app = electron.app;
+
+const loadPage = (page) => {
+    win.loadFile('renderer/pages/' + page)
+}
+
+let win
+const load = () => {
+     win = new electron.BrowserWindow({
+        width: 800,
+        height: 600
+    })
+}
+
+app.whenReady().then(() => {
+    load()
+    win.loadFile('renderer/main.html')
+})
